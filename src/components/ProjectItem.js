e0f45514ed35e990f731b52e92e15252dea52f8e
Margarita Villaseñor <maggie.villasenor@hotmail.com>
2023-05-12 15:21:10 -0700

    ci: added projects endpoint

--- src/components/ProjectItem.js
+++ src/components/ProjectItem.js
@@ -5,30 +5,30 @@ import Stack from "@mui/material/Stack";
 import Grid from "@mui/material/Grid";
 
 export default function ProjectItem({ project }) {
-  const router = useRouter();
+    const router = useRouter();
 
-  return (
-    <Grid container spacing={6} sx={{ pb: "40px" }}>
-      <Grid item md={6}>
-        <img
-          src={project.imageUrl}
-          alt={project.name}
-          style={{ width: "100%" }}
-        />
-      </Grid>
-      <Grid item md={6}>
-        <Stack spacing={4}>
-          <h3>{project.name}</h3>
-          <div>{project.description}</div>
-          <Button
-            variant="contained"
-            size="large"
-            onClick={() => router.push(`/projects/${project._id}`)}
-          >
-            Use Case
-          </Button>
-        </Stack>
-      </Grid>
-    </Grid>
+    return (
+        <Grid container spacing={6} sx={{ pb: "40px" }}>
+            <Grid item md={6}>
+                <img
+                    src={project.imageUrl}
+                    alt={project.name}
+                    style={{ width: "100%" }}
+                />
+            </Grid>
+            <Grid item md={6}>
+                <Stack spacing={4}>
+                    <h3>{project.name}</h3>
+                    <div>{project.description}</div>
+                    <Button
+                        variant="contained"
+                        size="large"
+                        onClick={() => router.push(`/projects/${project._id}`)}
+                    >
+                        Use Case
+                    </Button>
+                </Stack>
+            </Grid>
+        </Grid>
   );
 }
